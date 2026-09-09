935bf56743e5fff62c9bd3c877ba234d4302bb55
Karatesan <maciej.gomulec@gmail.com>
2026-09-09 12:11:32 +0200

    Update StatusVerb to use EquipmentSlot on Thrall

--- src/main/java/com/archon/verb/StatusVerb.java
+++ src/main/java/com/archon/verb/StatusVerb.java
@@ -1,5 +1,6 @@
 package com.archon.verb;
 
+import com.archon.model.EquipmentSlot;
 import com.archon.model.Inventory;
 import com.archon.model.Item;
 import com.archon.model.Thrall;
@@ -12,14 +13,14 @@ public final class StatusVerb extends FreeVerb {
     public ExitCode execute(VerbContext c) {
         Thrall t = c.thrall;
         c.say("HP " + t.hp + "/" + t.maxHp + "  tags " + t.tags
-                + "\n  hand/right: " + slot(t, "hand/right")
-                + "\n  hand/left : " + slot(t, "hand/left")
+                + "\n  hand/right: " + slot(t, EquipmentSlot.HAND_RIGHT)
+                + "\n  hand/left : " + slot(t, EquipmentSlot.HAND_LEFT)
                 + "\n  pack (" + t.inventory().pack().size() + "/" + Inventory.PACK_MAX + "): " + t.inventory().pack()
                 + "\n  nocked: " + t.nocked);
         return ExitCode.SUCCESS;
     }
 
-    private String slot(Thrall t, String s) {
+    private String slot(Thrall t, EquipmentSlot s) {
         Item i = t.getSlot(s);
         return i == null ? "empty" : i.name;
     }
