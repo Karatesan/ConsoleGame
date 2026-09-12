508d2cbe23b91c577370a5cc9da9482d4ef48e88
Karatesan <maciej.gomulec@gmail.com>
2026-09-13 00:30:56 +0200

    Update ConsoleView to use hp() and maxHp() accessors

--- src/main/java/com/archon/ui/ConsoleView.java
+++ src/main/java/com/archon/ui/ConsoleView.java
@@ -150,7 +150,7 @@ public final class ConsoleView implements View {
         return String.format("""
                         ROUND %d   HP %d/%d   AP [%s] %d/%d   LINE %d (next tax: +%d AP)
                         hand/right: %-18s hand/left: %-18s pack %d/%d
-                        """, r.roundNo(), t.hp, t.maxHp, pips.toString().trim(), r.ap(), RoundState.BASE_AP, r.linesUsed(),
+                        """, r.roundNo(), t.hp(), t.maxHp(), pips.toString().trim(), r.ap(), RoundState.BASE_AP, r.linesUsed(),
                 r.taxForNextLine(), name(t.inventory().getEquipped(EquipmentSlot.HAND_RIGHT)), name(t.inventory().getEquipped(EquipmentSlot.HAND_LEFT)), t.inventory().pack().size(),
                 Inventory.PACK_MAX);
     }
