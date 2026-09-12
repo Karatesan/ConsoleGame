3c772e3477b197340271e6c72d5d0a5002983b5d
Karatesan <maciej.gomulec@gmail.com>
2026-09-12 23:48:24 +0200

    Refactor Thrall to extend Actor and inherit combat and inventory systems

--- src/main/java/com/archon/model/Thrall.java
+++ src/main/java/com/archon/model/Thrall.java
@@ -1,31 +1,13 @@
 package com.archon.model;
 
-public final class Thrall extends Entity {
-    private final Inventory inventory = new Inventory();
-    public boolean nocked;
-    public int strength = 3;
+public final class Thrall extends Actor {
 
     public Thrall(Vec2 pos, int hp) {
         super("self", "Thrall", 'T', Kind.CREATURE, pos, hp);
+        strength = 3;
         tags.add(Tag.ORGANIC);
         tags.add(Tag.FLESH);
         tags.add(Tag.FLAMMABLE);
         identified = true;
-    }
-
-    public Inventory inventory() {
-        return inventory;
-    }
-
-    public Item findInPack(String idOrName) {
-        return inventory.findInPack(idOrName);
-    }
-
-    /**
-     * Creature combat behavior: attempts right hand first, falls back to left hand.
-     */
-    public Item mainHand() {
-        Item right = inventory.getEquipped(EquipmentSlot.HAND_RIGHT);
-        return right != null ? right : inventory.getEquipped(EquipmentSlot.HAND_LEFT);
     }
 }
