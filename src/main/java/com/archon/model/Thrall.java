cc7d3453967eee37505a661c326531ffbf87439d
Karatesan <maciej.gomulec@gmail.com>
2026-09-15 14:48:03 +0200

    Finalize thrall actor model

--- src/main/java/com/archon/model/Thrall.java
+++ src/main/java/com/archon/model/Thrall.java
@@ -5,9 +5,7 @@ public final class Thrall extends Actor {
     public Thrall(Vec2 pos, int hp) {
         super("self", "Thrall", 'T', pos, new CreatureStats(hp, 0, 0, 3));
 
-        applyTag(Tag.ORGANIC);
-        applyTag(Tag.FLESH);
-        applyTag(Tag.FLAMMABLE);
-        setIdentified(true);
+        with(Tag.ORGANIC, Tag.FLESH, Tag.FLAMMABLE);
+        identify();
     }
 }
