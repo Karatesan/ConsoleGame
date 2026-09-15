e33c04498bce043749447a518172fcaf60d57849
Karatesan <maciej.gomulec@gmail.com>
2026-09-15 14:03:09 +0200

    Make thrall use actor-owned state

--- src/main/java/com/archon/model/Thrall.java
+++ src/main/java/com/archon/model/Thrall.java
@@ -3,7 +3,7 @@ package com.archon.model;
 public final class Thrall extends Actor {
 
     public Thrall(Vec2 pos, int hp) {
-        super("self", "Thrall", 'T', pos, CreatureStats.of(hp, 0, 0, 3));
+        super("self", "Thrall", 'T', pos, new CreatureStats(hp, 0, 0, 3));
 
         applyTag(Tag.ORGANIC);
         applyTag(Tag.FLESH);
