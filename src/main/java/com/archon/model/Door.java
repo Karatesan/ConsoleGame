4854c70bfdcb5b5ec582ca306e1fc69916ab1342
Karatesan <maciej.gomulec@gmail.com>
2026-09-15 13:01:57 +0200

    Add door model

---
+++ src/main/java/com/archon/model/Door.java
@@ -0,0 +1,8 @@
+package com.archon.model;
+
+/** A destructible door entity. */
+public final class Door extends Entity {
+    public Door(String id, String name, char glyph, Vec2 pos, int hp) {
+        super(id, name, glyph, Kind.DOOR, pos, hp, 0, 0);
+    }
+}
