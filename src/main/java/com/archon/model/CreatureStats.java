dbb2bfa142e811c953bc6583c35f194ecfc2c33b
Karatesan <maciej.gomulec@gmail.com>
2026-09-15 14:03:07 +0200

    Make creature stats immutable

--- src/main/java/com/archon/model/CreatureStats.java
+++ src/main/java/com/archon/model/CreatureStats.java
@@ -1,54 +1,16 @@
 package com.archon.model;
 
 /**
- * Encapsulates base permanent creature attributes and progression.
+ * Encapsulates immutable base creature attributes used during initialization.
+ * Live health and defensive state are owned by the entity.
  */
-public final class CreatureStats {
-    private int maxHp;
-    private int armor;
-    private int evasion;
-    private int strength;
+public record CreatureStats(int maxHp, int armor, int evasion, int strength) {
 
-    public CreatureStats(int maxHp, int armor, int evasion, int strength) {
-        this.maxHp = maxHp;
-        this.armor = armor;
-        this.evasion = evasion;
-        this.strength = strength;
+    public CreatureStats(int maxHp, int armor, int evasion) {
+        this(maxHp, armor, evasion, 3);
     }
 
     public static CreatureStats of(int maxHp, int armor, int evasion, int strength) {
         return new CreatureStats(maxHp, armor, evasion, strength);
-    }
-
-    public int maxHp() {
-        return maxHp;
-    }
-
-    public int armor() {
-        return armor;
-    }
-
-    public int evasion() {
-        return evasion;
-    }
-
-    public int strength() {
-        return strength;
-    }
-
-    public void increaseMaxHp(int delta) {
-        this.maxHp += delta;
-    }
-
-    public void increaseArmor(int delta) {
-        this.armor += delta;
-    }
-
-    public void increaseEvasion(int delta) {
-        this.evasion += delta;
-    }
-
-    public void increaseStrength(int delta) {
-        this.strength += delta;
     }
 }
