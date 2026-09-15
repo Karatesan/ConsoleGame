a6ca09f8aa4d3e89a61ff1963de8bf590b334a74
Karatesan <maciej.gomulec@gmail.com>
2026-09-15 13:25:44 +0200

    Finalize door model

--- src/main/java/com/archon/model/Door.java
+++ src/main/java/com/archon/model/Door.java
@@ -2,7 +2,8 @@ package com.archon.model;
 
 /** A destructible door entity. */
 public final class Door extends Entity {
-    public Door(String id, String name, char glyph, Vec2 pos, int hp) {
+    public Door(String id, String name, char glyph, Vec2 pos, int hp, int armor) {
         super(id, name, glyph, Kind.DOOR, pos, hp, 0, 0);
+        setArmor(armor);
     }
 }
