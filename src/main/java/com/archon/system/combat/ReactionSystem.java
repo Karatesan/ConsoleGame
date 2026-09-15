6045f55ad888fb65e35a12d7a5b42db773d2c7a8
Karatesan <maciej.gomulec@gmail.com>
2026-09-15 13:09:11 +0200

    Finalize reaction state encapsulation

--- src/main/java/com/archon/system/combat/ReactionSystem.java
+++ src/main/java/com/archon/system/combat/ReactionSystem.java
@@ -16,11 +16,15 @@ public final class ReactionSystem {
      */
     public static Entity pendingInterrupt(World world, boolean thrallMoved) {
         if (world.thrall == null) return null;
+
         for (Entity e : world.entities.values()) {
             if (!e.alive() || e.readied() == null || e.isReadiedSpent()) continue;
+
             switch (e.readied().trigger()) {
                 case ON_ADJACENCY -> {
-                    if (e.pos().chebyshev(world.thrall.pos()) <= 1) return e;
+                    if (e.pos().chebyshev(world.thrall.pos()) <= 1) {
+                        return e;
+                    }
                 }
                 case ON_MOVEMENT_IN_LOS -> {
                     if (thrallMoved && SpatialService.lineOfSight(world.map, e.pos(), world.thrall.pos())) {
@@ -29,6 +33,7 @@ public final class ReactionSystem {
                 }
             }
         }
+
         return null;
     }
 
@@ -45,8 +50,8 @@ public final class ReactionSystem {
      */
     public static int resolveInterrupt(World world, Entity e) {
         e.spendReadied();
-        int dmg = e.readied().damage();
-        world.thrall.takeDamage(dmg);
-        return dmg;
+        int damage = e.readied().damage();
+        world.thrall.takeDamage(damage);
+        return damage;
     }
 }
