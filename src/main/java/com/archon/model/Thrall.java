9b569fa6b94e527d153fd3ffb86b7415bc19e457
Karatesan <maciej.gomulec@gmail.com>
2026-09-15 13:07:34 +0200

    Finalize thrall actor inheritance

--- src/main/java/com/archon/model/Thrall.java
+++ src/main/java/com/archon/model/Thrall.java
@@ -3,9 +3,7 @@ package com.archon.model;
 public final class Thrall extends Actor {
     public Thrall(Vec2 pos, int hp) {
         super("self", "Thrall", 'T', pos, new CreatureStats(hp, 0, 0, 3));
-        tags.add(Tag.ORGANIC);
-        tags.add(Tag.FLESH);
-        tags.add(Tag.FLAMMABLE);
-        identified = true;
+        with(Tag.ORGANIC, Tag.FLESH, Tag.FLAMMABLE);
+        identify();
     }
 }
