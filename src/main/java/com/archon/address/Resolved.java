2b05c5eeb5f8747c58c4d3ebc9feed904a533226
Karatesan <maciej.gomulec@gmail.com>
2026-09-16 21:28:24 +0200

    Use encapsulated thrall in resolution

--- src/main/java/com/archon/address/Resolved.java
+++ src/main/java/com/archon/address/Resolved.java
@@ -22,7 +22,7 @@ public sealed interface Resolved {
                 Vec2 pos = Address.resolveTileSpec(tile.spec(), world);
                 yield world.inBounds(pos) ? new OnTile(pos, tile.layer()) : null;
             }
-            case Address.InventoryAddr inventory -> resolveInventory(world.thrall, inventory.path(), "self");
+            case Address.InventoryAddr inventory -> resolveInventory(world.thrall(), inventory.path(), "self");
             case Address.EntityAddr entityAddress -> resolveEntity(entityAddress, world);
         };
     }
