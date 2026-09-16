8fc114705da409045e5f0c53490f709056c919df
Karatesan <maciej.gomulec@gmail.com>
2026-09-16 21:34:53 +0200

    Fix ignite map and spatial imports

--- src/main/java/com/archon/verb/IgniteVerb.java
+++ src/main/java/com/archon/verb/IgniteVerb.java
@@ -3,9 +3,10 @@ package com.archon.verb;
 import com.archon.address.Resolved;
 import com.archon.command.Ast;
 import com.archon.model.Entity;
-import com.archon.model.SpatialService;
+import com.archon.model.GameMap;
 import com.archon.model.Tag;
 import com.archon.model.Vec2;
+import com.archon.system.spatial.SpatialService;
 
 public final class IgniteVerb implements Verb {
     @Override
@@ -70,9 +71,9 @@ public final class IgniteVerb implements Verb {
 
         if (resolved instanceof Resolved.OnTile onTile) {
             final Vec2 at = onTile.pos();
+            final GameMap.Tile tile = c.world.tile(at);
 
-            if (!c.world.map().hasTag(at, Tag.OIL)
-                    && !c.world.map().hasTag(at, Tag.FLAMMABLE)) {
+            if (!tile.has(Tag.OIL) && !tile.has(Tag.FLAMMABLE)) {
                 c.say("nothing to burn at " + at);
                 return ExitCode.MISS;
             }
