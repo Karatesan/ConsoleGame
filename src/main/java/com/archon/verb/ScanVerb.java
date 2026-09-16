346d6891f3eccde5295daed1540f455d80b41e96
Karatesan <maciej.gomulec@gmail.com>
2026-09-16 20:59:08 +0200

    Scan actor-owned reactions

--- src/main/java/com/archon/verb/ScanVerb.java
+++ src/main/java/com/archon/verb/ScanVerb.java
@@ -1,5 +1,6 @@
 package com.archon.verb;
 
+import com.archon.model.Actor;
 import com.archon.model.Entity;
 import com.archon.model.Tag;
 
@@ -31,7 +32,9 @@ public final class ScanVerb extends FreeVerb {
                     e.hp(),
                     e.maxHp(),
                     e.tags().contains(Tag.BURNING) ? "[BURNING] " : "",
-                    e.readied() != null ? "[READIED: " + e.readied().description() + "] " : "",
+                    e instanceof Actor actor && actor.readied() != null
+                            ? "[READIED: " + actor.readied().description() + "] "
+                            : "",
                     e.pos().chebyshev(c.thrall.pos()) <= 1
                             ? "adjacent"
                             : "range " + e.pos().chebyshev(c.thrall.pos())
